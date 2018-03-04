20cac8a33959851df979fd14753cb2679c8ba546
Alexander Chaban <alex-chaban@mail.ru>
2018-03-04 15:34:35 +0300

    text analyzer / fix import

--- src/main/java/com/alex/test/analyzer/Application.java
+++ src/main/java/com/alex/test/analyzer/Application.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.alex.test.analyzer")
