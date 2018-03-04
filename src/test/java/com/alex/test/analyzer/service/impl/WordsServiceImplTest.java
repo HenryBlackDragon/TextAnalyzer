a26736386c91f82e22c0d5a5472bcf7c6cdc7321
Alexander Chaban <alex-chaban@mail.ru>
2018-03-04 15:46:56 +0300

    text analyzer / add test

--- src/test/java/com/alex/test/analyzer/service/impl/WordsServiceImplTest.java
+++ src/test/java/com/alex/test/analyzer/service/impl/WordsServiceImplTest.java
@@ -1,17 +1,45 @@
 package com.alex.test.analyzer.service.impl;
 
+import com.alex.test.analyzer.model.Word;
+import com.alex.test.analyzer.service.WordsService;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 
 public class WordsServiceImplTest {
 
+    private WordsService wordsService;
 
-    @Test
-    public void getListWordsTest() {
+    @Before
+    public void setUp() throws Exception {
+        wordsService = new WordsServiceImpl();
     }
 
     @Test
-    public void checkFileTest() {
+    public void getListWordsTest() throws URISyntaxException {
+        List<Word> result = wordsService.getListWords(String.valueOf(
+                Paths.get(ClassLoader.getSystemResource("test_text.txt").toURI())));
+        assertNotNull(result);
+        assertEquals("это", result.get(0).getName());
+        assertEquals("1", result.get(0).getCount().toString());
+        assertEquals("тестовый", result.get(1).getName());
+        assertEquals("1", result.get(1).getCount().toString());
+        assertEquals("файл", result.get(2).getName());
+        assertEquals("1", result.get(2).getCount().toString());
+    }
+
+    @Test
+    public void checkFileTest() throws URISyntaxException {
+        String result = wordsService.checkFile(String.valueOf(
+                Paths.get(ClassLoader.getSystemResource("test_text.txt").toURI())));
+        assertNotNull(result);
+        assertEquals("Correct", result);
     }
 }
